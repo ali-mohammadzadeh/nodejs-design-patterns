173afc73f36a44b15927f4ee3ed7bc2511d57245
ali <alimohammadzade662@gmail.com>
2022-03-05 00:51:19 +0330

    implementation factory

---
+++ factory/factory.js
@@ -0,0 +1,38 @@
+class Developer {
+    constructor(username) {
+        this.username = username
+    }
+}
+
+class Tester {
+    constructor(username) {
+        this.username = username
+    }
+}
+
+
+class EmployeeFactory {
+   static map = {
+        "developer": Developer,
+        "tester":Tester,
+    }
+
+    static create(username,type) {
+        return new this.map[type](username)
+    }
+}
+
+
+const employees = [];
+
+employees.push(EmployeeFactory.create("mahnaz", "developer"))
+employees.push(EmployeeFactory.create("reihaneh", "developer"))
+employees.push(EmployeeFactory.create("ali", "developer"))
+
+employees.push(EmployeeFactory.create("sara", "tester"))
+employees.push(EmployeeFactory.create("niloufar", "tester"))
+
+
+employees.forEach((employee) => {
+    console.log(employee);
+});
